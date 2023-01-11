2596f7bef7d69b156e33b261efeb718ad6ab9785
Marvin Smith <marvin.smith@sncorp.com>
2023-01-11 23:42:44 +0000

    Shunting Yard, not Shunting Tree.  Why cant i remember that?

--- src/math/Postfix2Infix.cpp
+++ src/math/Postfix2Infix.cpp
@@ -5,12 +5,32 @@
  */
 #include "Postfix2Infix.hpp"
 
+// C++ Libraries
+#include <deque>
+
 /*************************************************************************************/
 /*          Convert Postfix to Infix Notation using Shunting Yard algorithm          */
 /*************************************************************************************/
 std::vector<Token> Postfix2Infix_ShuntingYard( const std::vector<Token>& tokens )
 {
+    std::vector<Token> output_queue;
+    std::deque<Token> operator_stack;
+
     // Iterate over each token
+    for( const auto& token : tokens )
+    {
+        // If token is number, add to output queue
+        if( token.Get_Type() == TOKEN_TYPE::NUMBER )
+        {
+            output_queue.push_back( token );
+        }
+
+        // If the token is a function, push to operator stack
+
+
+        // If the token is an operator
+    }
+
 
 }
 
